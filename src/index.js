78279921fa27731d1832921606a86bae0045c90e
GustiGetTha <4gustiagung@gmail.com>
2024-02-17 23:26:53 +0800

    Menghapus komentar pada index

--- src/index.js
+++ src/index.js
@@ -4,38 +4,6 @@ const experss = require('express');
 
 const app = experss();
 
-// app.method(path, handler);
-
-// use untuk middleware atau general method
-// app.use("/", (req, res, next) => {
-    //     res.send("Hello World");
-    // });
-    
-    // log setiap request
-    // app.use((req, res, next) => {
-        //     console.log('log terjadi request ke API ini');
-        //     next();
-        // })
-        
-        // app.get("/", (req, res) => {
-            //     res.send("GET Method");
-            // })
-            
-            app.post("/post", (req, res) => {
-                res.send("POST Method");
-            });
-            
-            app.get("/html", (req, res) => {
-                res.send("<h1>H1 HTML</h1>");
-            });
-            
-            app.get("/json", (req, res) => {
-                res.json({
-        nama: "Jason",
-        alamat: "Samarinda"
-    });
-});
-
 //=================================
 const middlewareLogRequest = require('./middlewares/logs.js');
 app.use(middlewareLogRequest);
